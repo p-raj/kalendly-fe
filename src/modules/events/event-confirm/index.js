702394c95ca355f6abf9daf7b491eb61d7b2d192
PR <peeyush.raj@gmail.com>
2021-05-04 18:27:40 +0530

    code refactoring for react key warning

--- src/modules/events/event-confirm/index.js
+++ src/modules/events/event-confirm/index.js
@@ -11,8 +11,6 @@ dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(localizedFormat);
 
-import { v4 as uuid } from "uuid";
-
 import data from "./data";
 
 class EventConfirmation extends Component {
@@ -130,9 +128,7 @@ class EventConfirmation extends Component {
 
     renderDivider = () => {
         return (
-            <div
-                key={uuid()}
-                className="w-full border-gray-200 border-0 border-b border-solid"></div>
+            <div className="w-full border-gray-200 border-0 border-b border-solid"></div>
         );
     };
 
@@ -146,76 +142,58 @@ class EventConfirmation extends Component {
 
         if (!this.state.smallScreen) {
             return (
-                <div className="grid grid-cols-4 gap-4 px-16 py-10">
-                    <h3 key="headers-name">{headers.name}</h3>
-                    <h3 key="headers-email">{headers.email}</h3>
-                    <h3 key="headers-timezone">{headers.timezone}</h3>
-                    <h3 key="headers-status">{headers.status}</h3>
-                    {this.renderDivider()}
-                    {this.renderDivider()}
-                    {this.renderDivider()}
-                    {this.renderDivider()}
+                <>
+                    <div className="grid grid-cols-4 gap-4 px-16 py-10">
+                        <h3>{headers.name}</h3>
+                        <h3>{headers.email}</h3>
+                        <h3>{headers.timezone}</h3>
+                        <h3>{headers.status}</h3>
+                        {this.renderDivider()}
+                        {this.renderDivider()}
+                        {this.renderDivider()}
+                        {this.renderDivider()}
+                    </div>
                     {guestList.map((guest, index) => {
                         return (
-                            <>
-                                <div key={`${guest.name}-${index}`}>
-                                    {guest.name}
-                                </div>
-                                <div key={`${guest.email}-${index}`}>
-                                    {guest.email}
-                                </div>
-                                <div key={`${guest.timezone}-${index}`}>
-                                    {guest.timezone}
-                                </div>
-                                <div key={`${guest.status}-${index}`}>
-                                    {guest.status}
-                                </div>
-                            </>
+                            <div
+                                key={index}
+                                className="grid grid-cols-4 gap-4 px-16">
+                                <div>{guest.name}</div>
+                                <div>{guest.email}</div>
+                                <div>{guest.timezone}</div>
+                                <div>{guest.status}</div>
+                            </div>
                         );
                     })}
-                </div>
+                </>
             );
         } else {
             return (
-                <div className="grid grid-cols-2 gap-4 px-4 py-4">
+                <>
                     {guestList.map((guest, index) => {
                         return (
-                            <>
+                            <div
+                                key={index}
+                                className="grid grid-cols-2 gap-4 px-4 py-4">
                                 {/* name */}
-                                <h3 key={`headers-name-${index}`}>
-                                    {headers.name}
-                                </h3>
-                                <div key={`${guest.name}-${index}`}>
-                                    {guest.name}
-                                </div>
+                                <h3>{headers.name}</h3>
+                                <div>{guest.name}</div>
                                 {/* email */}
-                                <h3 key={`headers-email-${index}`}>
-                                    {headers.email}
-                                </h3>
-                                <div key={`${guest.email}-${index}`}>
-                                    {guest.email}
-                                </div>
+                                <h3>{headers.email}</h3>
+                                <div>{guest.email}</div>
                                 {/* timezone */}
-                                <h3 key={`headers-timezone-${index}`}>
-                                    {headers.timezone}
-                                </h3>
-                                <div key={`${guest.timezone}-${index}`}>
-                                    {guest.timezone}
-                                </div>
+                                <h3>{headers.timezone}</h3>
+                                <div>{guest.timezone}</div>
                                 {/* status */}
-                                <h3 key={`headers-status-${index}`}>
-                                    {headers.status}
-                                </h3>
-                                <div key={`${guest.status}-${index}`}>
-                                    {guest.status}
-                                </div>
+                                <h3>{headers.status}</h3>
+                                <div>{guest.status}</div>
                                 {/* divider */}
                                 {this.renderDivider()}
                                 {this.renderDivider()}
-                            </>
+                            </div>
                         );
                     })}
-                </div>
+                </>
             );
         }
     };
