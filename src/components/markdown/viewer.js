ca2a71111cbe90b64873faab62cdb581cc33768e
PR <peeyush.raj@gmail.com>
2021-05-04 17:31:06 +0530

    Code refactoring around editor

--- src/components/markdown/viewer.js
+++ src/components/markdown/viewer.js
@@ -3,10 +3,9 @@ import PropTypes from "prop-types";
 import MDEditor from "@uiw/react-md-editor";
 
 export default function Viewer(props) {
-    const [value, setValue] = React.useState(props.value);
     return (
         <div className="container">
-            <MDEditor.Markdown source={value} />
+            <MDEditor.Markdown source={props.value} />
         </div>
     );
 }
