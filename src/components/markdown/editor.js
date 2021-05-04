a983757263c74cba6ea7b06437141c60bfb9e4d4
PR <peeyush.raj@gmail.com>
2021-05-04 16:57:28 +0530

    Editor Changes

--- src/components/markdown/editor.js
+++ src/components/markdown/editor.js
@@ -1,33 +1,11 @@
 import React from "react";
-import { Editor, EditorState, RichUtils } from "draft-js";
+import MDEditor from "@uiw/react-md-editor";
 
-class MyEditor extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { editorState: EditorState.createEmpty() };
-        this.onChange = (editorState) => this.setState({ editorState });
-        this.handleKeyCommand = this.handleKeyCommand.bind(this);
-    }
-
-    handleKeyCommand(command, editorState) {
-        const newState = RichUtils.handleKeyCommand(editorState, command);
-
-        if (newState) {
-            this.onChange(newState);
-            return "handled";
-        }
-
-        return "not-handled";
-    }
-
-    render() {
-        return (
-            <Editor
-                editorState={this.state.editorState}
-                handleKeyCommand={this.handleKeyCommand}
-                onChange={this.onChange}
-            />
-        );
-    }
+export default function Editor() {
+    const [value, setValue] = React.useState("**Hello world!!!**");
+    return (
+        <div className="container">
+            <MDEditor value={value} onChange={setValue} />
+        </div>
+    );
 }
-export default MyEditor;
